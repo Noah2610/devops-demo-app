857306f60064eb8d97f0c071567331513c4204b4
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2023-05-31 19:07:33 +0200

    devops-demo-updater: Update update.sh script path

--- devops-demo-updater/server.js
+++ devops-demo-updater/server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const HOST = process.env.HOST || "0.0.0.0";
 const PORT = process.env.PORT || 8081;
 const UPDATE_SCRIPT = path.resolve(
-    __dirname + "/../devops-demo-app/bin/update.sh",
+    __dirname + "/../bin/update.sh",
 );
 
 let isRunningScript = false;
